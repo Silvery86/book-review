bd655a1208577d022554c9959d8f7fa9cd2dc962
giang <giang8692@gmail.com>
2025-02-18 23:16:18 +0700

    Add: Book and Review factory and seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Book;
+use App\Models\Review;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +14,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Book::factory(33)->create()->each(function (Book $book) {
+            $numReviews = random_int(5,30);
+
+            Review::factory()->count($numReviews)
+            ->good()
+            ->for($book)
+            ->create();
+        });
+
+        Book::factory(33)->create()->each(function ( Book $book) {
+            $numReviews = random_int(5,30);
+
+            Review::factory()->count($numReviews)
+            ->average()
+            ->for($book)
+            ->create();
+        });
+
+        Book::factory(34)->create()->each(function ( Book $book) {
+            $numReviews = random_int(5,30);
+
+          Review::factory()->count($numReviews)
+            ->bad()
+            ->for($book)
+            ->create();
+        });
     }
 }
